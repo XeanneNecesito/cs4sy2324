c285c5834ab025d17bc580978fd0ed0518464535
XeanneNecesito <112594398+XeanneNecesito@users.noreply.github.com>
2023-08-31 11:35:22 +0800

    Update CS4CharmNecesito.java

--- src/cs4_charm_necesito/CS4CharmNecesito.java
+++ src/cs4_charm_necesito/CS4CharmNecesito.java
@@ -16,8 +16,22 @@ public class CS4CharmNecesito {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String greeting = "Hello World";
-        System.out.println(greeting);
+        String btype = "Blood type";
+        String name = "Name: ";
+        String color = "Color: ";
+        String count = "Cell count in a drop of blood: ;
+
+        //blood type1
+        System.out.println(btype) + "1";
+        //put new line here
+        String rbc = "Red Blood Cells";
+        String rbcolor = "Red";
+        String rbcount = "5000000";
+        System.out.println(name) + System.out.println(rbc);
+        System.out.println(color) + System.out.println(rbcolor);
+        System.out.println(count) + System.out.println(rbcount);
+        
+        
     }
     
 }
